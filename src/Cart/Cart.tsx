d8c1b40e69e1f3414cbd9f58f8c58db77ee4a176
Summers Wu <lwu259@uwo.ca>
2022-03-03 23:38:33 -0500

    payments mvp

--- src/Cart/Cart.tsx
+++ src/Cart/Cart.tsx
@@ -3,6 +3,14 @@ import {Wrapper} from './Cart.styles';
 import {CartItemType} from '../Pages/MenuPage';
 import Button from '@material-ui/core/Button';
 import React from 'react';
+import { loadStripe } from '@stripe/stripe-js';
+
+var stripe : any = null;
+
+(async function() {
+  stripe = await loadStripe('pk_test_51HUmg1ABLPX73TqOgs3vYP0Q5cpY9NWY789qfksUzq1qE6cIjAgdCZgCbfcsrvVbE3VSTdTLGdWfLtpolPhHh1gf00XypK7Dhj');
+  // → 🎉
+}());
 
 type Props = {
     cartItems: CartItemType[];
@@ -28,7 +36,6 @@ async function sendOrder(order: CartItemType[]){
   //console.log(orderProducts);
   
    await fetch("http://localhost:8080/api/order", {
-			
 			method: "POST",
 			body:JSON.stringify({orderProducts:orderProducts,storeId:storeId,tableId:tableId}),
 			headers: {
@@ -56,6 +63,35 @@ async function sendOrder(order: CartItemType[]){
       else{
         return true;
       }
+}
+
+async function payForOrder(order: CartItemType[]){
+  var orderProducts:any=[];
+  var storeId:string;
+  var tableId:string='61a79d4777fabcc990bbbd62';
+  storeId=order[0].storeId;
+  console.log(order)
+
+  order.map(item => {
+    orderProducts.push({
+      productName: item.productName,
+      productPrice: item.productPrice,
+      productImageUrl: item.productImageUrl,
+      productDescription: item.productDescription,
+      productAmount: item.amount
+    })
+  });
+  const res = await fetch("http://localhost:8080/api/payment/create-checkout-session", {
+			method: "POST",
+      mode: 'cors',
+			body:JSON.stringify({orderProducts:orderProducts,storeId:storeId,tableId:tableId}),
+			headers: {
+				"Content-Type": "application/json",
+			},
+	})
+  const body = await res.json()
+  window.location.href = body.url
+  return true
 }
  
   const Cart: React.FC<Props> = ({ cartItems, addToCart, removeFromCart }) => {
@@ -85,16 +121,30 @@ async function sendOrder(order: CartItemType[]){
           />
         ))}
         <h2>Total: ${calculateTotal(cartItems).toFixed(2)}</h2>
-        {cartItems.length===0? <p>Select items from the menu to order</p>:<Button disabled={disable} 
-          onClick={async ()=>{
-          setDisable(true);
-          confirmationText=await sendOrder(cartItems);
-            console.log(confirmationText)
-          }}>
-          Complete Order</Button>}
+        {cartItems.length===0
+          ? <p>Select items from the menu to order</p>
+          :<Button disabled={disable} 
+            onClick={async ()=>{
+              setDisable(true);
+              confirmationText=await sendOrder(cartItems);
+              console.log(confirmationText)
+            }}>
+            Complete Order
+          </Button>
+        }
+        {cartItems.length===0
+          ? <p></p>
+          :<Button disabled={disable} 
+              onClick={async ()=>{
+                setDisable(true);
+                confirmationText=await payForOrder(cartItems);
+                console.log(confirmationText)
+              }}>
+              Pay For Order
+           </Button>
+        }
           {confirmationText===false? null:
             cartItems.length===0? null :<p>Your order has been sent to the kitchen</p>}
-
       </Wrapper>
     );
   };
